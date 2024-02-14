b488df30ddeb3a9fdb80cdb3903a98d382293acb
kult9pka <kult9pka@gmail.com>
2024-02-14 14:44:00 +1000

    gtest_test

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,6 @@
-#define BOOST_TEST_MODULE test_version
-
 #include "lib.h"
+#include <gtest/gtest.h>
 
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() > 0);
+TEST(VersionTest, Positive) {
+    EXPECT_GT(version(), 0);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
